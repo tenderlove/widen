85ebbf58ca68925ecad68c251d13b6034f08d4b1
Aaron Patterson <aaron.patterson@gmail.com>
2016-06-23 14:37:01 +0800

    add mutation methods

--- test/test_widen.rb
+++ test/test_widen.rb
@@ -2,8 +2,18 @@ require 'minitest/autorun'
 require 'widen'
 
 class TestWiden < Minitest::Test
-  def test_sanity
+  def test_no_mutation
     assert_equal 'ＡＢＣＤＥ', 'abcde'.upcase.widen
     assert_equal 'abcde', 'ＡＢＣＤＥ'.narrowen.downcase
+  end
+
+  def test_mutation
+    x = 'abcde'.upcase
+    x.widen!
+    assert_equal 'ＡＢＣＤＥ', x
+
+    x = 'ＡＢＣＤＥ'.downcase
+    x.narrowen!
+    assert_equal 'abcde', x
   end
 end
